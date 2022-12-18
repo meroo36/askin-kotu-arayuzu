28860350d6548c7636d64800d939ea11a89becb4
Mert Doğruca <a7xlife@hotmail.com>
2022-12-18 21:47:03 +0300

    update absolute url

--- utils/absoluteUrl.ts
+++ utils/absoluteUrl.ts
@@ -3,6 +3,6 @@ export function getAbsoluteUrl() {
         return 'http://localhost:3000'
     }
     else if (process.env.NODE_ENV == 'production') {
-        return process.env.VERCEL_URL
+        return process.env.VERCEL_URL ?? 'http://localhost:3000'
     }
 }
